3d47927e4823036d606f0997c1b749bf0222784f
krisna <krisnamadani@gmail.com>
2024-07-15 12:31:43 +0700

    rating & review count on all pages

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,12 +26,12 @@ class BookController extends Controller
             'popular_last_6months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
 
         $cacheKey = 'books:' . $filter . ':' . $title;
         
-        $books = Cache::remember($cacheKey, 3600, fn() => $books->get());
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
@@ -55,13 +55,13 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
-        $cacheKey = 'book:' . $book->id;
+        $cacheKey = 'book:' . $id;
 
-        $book = Cache::remember($cacheKey, 3600, fn() => $book->load([
+        $book = Cache::remember($cacheKey, 3600, fn() => Book::with([
             'reviews' => fn($query) => $query->latest()
-        ]));
+        ]))->withAvgRating()->withReviewsCount()->findOrFail($id);
 
         return view('books.show', ['book' => $book]);
     }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
 
 Route::resource('books', BookController::class);
