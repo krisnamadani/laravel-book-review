f5fa18f2f05e4c343cd0af8057a18be42c316782
krisna <krisnamadani@gmail.com>
2024-07-15 12:23:17 +0700

    invalidating cache

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Review extends Model
 {
@@ -14,5 +15,11 @@ class Review extends Model
     public function book()
     {
         return $this->belongsTo(Book::class);
+    }
+
+    protected static function booted()
+    {
+        static::updated(fn(Review $review) => Cache::forget('book:' . $review->book_id));
+        static::deleted(fn(Review $review) => Cache::forget('book:' . $review->book_id));
     }
 }
